f4cc1d555dbb547c27b3c3bb430761254f5f99c7
Esteban Mosquera <estebanmosque29@gmail.com>
2026-04-21 19:42:24 -0500

    Add EstadisticaJugador and Jugador class headers
    ...

---
+++ archivos .h
@@ -0,0 +1,49 @@
+EstadisticaJugador.h
+
+#ifndef ESTADISTICAJUGADOR_H
+#define ESTADISTICAJUGADOR_H
+
+class EstadisticaJugador {
+private:
+    int partidos;
+    int goles;
+
+public:
+    EstadisticaJugador();
+    EstadisticaJugador(const EstadisticaJugador& otra);
+
+    void setGoles(int g);
+    int getGoles();
+
+    EstadisticaJugador operator+(const EstadisticaJugador& otra);
+};
+
+#endif
+
+
+Jugador.h
+
+#ifndef JUGADOR_H
+#define JUGADOR_H
+
+#include "EstadisticaJugador.h"
+
+class Jugador {
+private:
+    char* nombre;
+    int numero;
+    EstadisticaJugador* estadisticas;
+
+public:
+    Jugador();
+    Jugador(const char* nom, int num);
+    Jugador(const Jugador& otro);
+    ~Jugador();
+
+    void setNombre(const char* nom);
+    char* getNombre();
+
+    bool operator==(const Jugador& otro);
+};
+
+#endif
